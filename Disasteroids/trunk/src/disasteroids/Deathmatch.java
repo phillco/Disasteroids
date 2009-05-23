99caca563b40de66dc4b3bad032130ea4b32ced5
Phillip Cohen <pc@phillipcohen.net>
2009-05-23 23:43:22 +0000

    In Deathmatch mode, a few black holes are randomly spawned. Bonus appear randomly right above each hole during the game.

--- Disasteroids/trunk/src/disasteroids/Deathmatch.java
+++ Disasteroids/trunk/src/disasteroids/Deathmatch.java
@@ -4,16 +4,13 @@
  */
 package disasteroids;
 
-import disasteroids.gui.AsteroidsFrame;
-import disasteroids.gui.RelativeGraphics;
+import disasteroids.gui.Particle;
+import disasteroids.gui.ParticleManager;
 import java.awt.Color;
-import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import javax.swing.JOptionPane;
 
 /**
  * A game mode where players fend off waves of asteroids.
@@ -24,15 +21,34 @@ public class Deathmatch implements GameMode
 
     public Deathmatch()
     {
+        // Create some random black holes.
+        for ( int i = 0, numHoles = Util.getRandomGenerator().nextInt( 2 ) + 2; i < numHoles; i++ )
+            Game.getInstance().getObjectManager().addObject( new BlackHole( Util.getRandomGenerator().nextInt( Game.getInstance().GAME_WIDTH ), Util.getRandomGenerator().nextInt( Game.getInstance().GAME_HEIGHT ), 13, -1 ) );
     }
 
     public void act()
     {
+        // Spawn bonuses at the black holes.
+        for ( BlackHole b : Game.getInstance().getObjectManager().getBlackHoles() )
+        {
+            if ( Util.getRandomGenerator().nextInt( 1500 ) == 0 )
+            {
+                Game.getInstance().getObjectManager().addObject( new Bonus( b.getX(), b.getY() - 150, Util.getRandomGenerator().nextInt( 8 ) - 4, 0 ) );
+                for ( int i = 0; i < 6; i++ )
+                    ParticleManager.addParticle( new Particle(
+                            b.getX() + Util.getRandomGenerator().nextInt( 8 ) - 4,
+                            ( b.getY() - 150 ) + Util.getRandomGenerator().nextInt( 8 ) - 4,
+                            Util.getRandomGenerator().nextInt( 4 ),
+                            Color.white,
+                            Util.getRandomGenerator().nextDouble() * 3,
+                            Util.getRandomGenerator().nextAngle(),
+                            50, 1 ) );
+            }
+        }
     }
 
     public void draw( Graphics g )
     {
-        
     }
 
     int getWavePoints( int wave )
@@ -42,16 +58,13 @@ public class Deathmatch implements GameMode
 
     public void flatten( DataOutputStream stream ) throws IOException
     {
-
     }
 
     public Deathmatch( DataInputStream stream ) throws IOException
     {
-
     }
 
     public void optionsKey()
     {
-
     }
 }

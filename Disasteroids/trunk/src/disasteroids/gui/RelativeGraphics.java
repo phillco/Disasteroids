cded4444a0089a9a07f7b5113aadb10c0117a5a4
Phillip Cohen <pc@phillipcohen.net>
2009-07-04 21:17:13 +0000

    Cleaned up RelativeGraphics.

--- Disasteroids/trunk/src/disasteroids/gui/RelativeGraphics.java
+++ Disasteroids/trunk/src/disasteroids/gui/RelativeGraphics.java
@@ -5,7 +5,6 @@
 package disasteroids.gui;
 
 import disasteroids.game.*;
-import disasteroids.Main;
 
 /**
  * Utility class for drawing graphics around the localPlayer.
@@ -14,21 +13,6 @@ import disasteroids.Main;
  */
 public abstract class RelativeGraphics
 {
-    public static int translateX( double x )
-    {
-        try
-        {
-            return ( int ) Math.round( ( x - Local.getLocalPlayer().getX() + MainWindow.frame().getWidth() / 2 + 4 * Game.getInstance().GAME_WIDTH ) % Game.getInstance().GAME_WIDTH ) + MainWindow.frame().getRumbleX();
-
-        }
-        catch ( NullPointerException nullPointerException )
-        {
-            Main.warning( "Null Pointer Exception, translateX" );
-            return -1;
-        }
-
-    }
-
     public static int translateX( double x, double width )
     {
         double newX = Math.round( ( x - Local.getLocalPlayer().getX() + MainWindow.frame().getWidth() / 2 + 4 * Game.getInstance().GAME_WIDTH ) % Game.getInstance().GAME_WIDTH + MainWindow.frame().getRumbleX() - width / 2 );
@@ -40,17 +24,9 @@ public abstract class RelativeGraphics
         return ( int ) newX;
     }
 
-    public static int translateY( double y )
+    public static int translateX( double x )
     {
-        try
-        {
-            return ( int ) Math.round( ( y - Local.getLocalPlayer().getY() + MainWindow.frame().getHeight() / 2 + 4 * Game.getInstance().GAME_HEIGHT ) % Game.getInstance().GAME_HEIGHT ) + MainWindow.frame().getRumbleY();
-        }
-        catch ( NullPointerException nullPointerException )
-        {
-            Main.warning( "Null Pointer Exception, translateY" );
-            return -1;
-        }
+        return translateX( x, 0 );
     }
 
     public static int translateY( double y, double height )
@@ -64,39 +40,24 @@ public abstract class RelativeGraphics
         return ( int ) newY;
     }
 
-    /**
-     * Calculates and returns the location directly opposite the first player
-     * @return the x coordinate of the location directly opposite the first player
-     */
-    public static int oppositeX()
+    public static int translateY( double y )
     {
-        try
-        {
-            return ( int ) ( ( Game.getInstance().getObjectManager().getPlayers().peek().getX() + Game.getInstance().GAME_WIDTH / 2 ) ) % Game.getInstance().GAME_WIDTH;
-
-        }
-        catch ( Exception exception )
-        {
-            Main.warning( exception.getClass().getName() + " at oppositeX" );
-            return -1;
-        }
-
+        return translateY( y, 0 );
     }
 
     /**
-     * Calculates and returns the location directly opposite the first player
-     * @return the y coordinate of the location directly opposite the first player
+     * Returns the location directly opposite the first player.
+     */
+    public static int oppositeX()
+    {
+        return ( int ) ( ( Game.getInstance().getObjectManager().getPlayers().peek().getX() + Game.getInstance().GAME_WIDTH / 2 ) ) % Game.getInstance().GAME_WIDTH;
+    }
+
+    /**
+     * Rreturns the location directly opposite the first player.
      */
     public static int oppositeY()
     {
-        try
-        {
-            return ( int ) ( Game.getInstance().getObjectManager().getPlayers().peek().getY() + Game.getInstance().GAME_HEIGHT / 2 ) % Game.getInstance().GAME_HEIGHT;
-        }
-        catch ( Exception exception )
-        {
-            Main.warning( exception.getClass().getName() + " at oppositeY" );
-            return -1;
-        }
+        return ( int ) ( Game.getInstance().getObjectManager().getPlayers().peek().getY() + Game.getInstance().GAME_HEIGHT / 2 ) % Game.getInstance().GAME_HEIGHT;
     }
 }
